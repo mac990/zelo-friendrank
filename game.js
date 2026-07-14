f4093d9d3d3ebc1b4cc4ef5576cb71d496c91a04
mac990 <mac@zelosportivo.com>
2026-07-14 18:47:06 +0800

    Update game.js

--- game.js
+++ game.js
@@ -40,7 +40,7 @@
    * =========================================================
    */
 
-  const VERSION = "202607141540-arena-logo-img-fixed";
+  const VERSION = "202607141845-arena-logo-img-fixed";
 
   const BG_IMAGE_URL =
     "https://cdn.shopify.com/s/files/1/0798/9844/4087/files/logo_34222be0-3841-4f77-b316-61efd088c633.png?v=1783871764";
