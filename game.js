d547c2006f71787e55b4470a7b0927719c08fe72
mac990 <mac@zelosportivo.com>
2026-07-17 06:28:05 +0800

    Update game.js

--- game.js
+++ game.js
@@ -51,7 +51,7 @@
 
  const VERSION = "202607170608-coupon-no-clip";
   
-  console.log(`[ZELO GAME] version: ${VERSION}`);
+  console.log("[ZELO LIQUID] loading game assets v=202607170608-coupon-no-clip");
   
   const BG_IMAGE_URL = "https://cdn.shopify.com/s/files/1/0798/9844/4087/files/logo_34222be0-3841-4f77-b316-61efd088c633.png?v=1783871764";
 
