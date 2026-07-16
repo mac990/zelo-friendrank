9f0f5603cbc3aa734f828091817bb4bb1327b242
mac990 <mac@zelosportivo.com>
2026-07-17 05:08:01 +0800

    Update game.js

--- game.js
+++ game.js
@@ -49,7 +49,7 @@
   const DEFAULT_TOP_IMAGE =
   "https://cdn.shopify.com/s/files/1/0798/9844/4087/files/whell.png?v=202607170240";
 
- const VERSION = "202607170451-result-ticket-layout";
+ const VERSION = "202607170506-result-ticket-title-fix";
   
   console.log(`[ZELO GAME] version: ${VERSION}`);
   
