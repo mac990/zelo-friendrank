7b0df58c1214b79dbfca591489ddee84dbdefbc8
mac990 <mac@zelosportivo.com>
2026-08-16 10:49:52 +0800

    Update game.js

--- game.js
+++ game.js
@@ -91,7 +91,7 @@ var markShareCompleted = window.markShareCompleted;
 
   const DEFAULT_TOP_IMAGE =
   "https://cdn.shopify.com/s/files/1/0798/9844/4087/files/whell.png?v=202607170240";
-  const VERSION = "202608160946-og-force-v15";
+  const VERSION = "202608161047-og-force-v16";
   console.log("[ZELO GAME] version:", VERSION);
 
   const HOME_MUSIC_URL =
